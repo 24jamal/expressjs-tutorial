c1410ad58093402b2b9679e065c095fc61fc53f5
jamaludeen750@gmail.com <jamal@MyPC.>
2026-02-07 15:36:10 +0000

    Advanced Routing

--- routes/users.js
+++ routes/users.js
@@ -7,8 +7,57 @@ router.get("/", (req, res) => {
     res.send("Users list");
 })
 
-router.get("/new", (req, res) => {
-    res.send("User One");
+
+
+router.post("/", (req, res) => {
+    res.send("User Created");
 })
+
+router
+    .route("/:id")
+    .get((req, res) => {
+        const id = req.params.id;
+        console.log("params:", req.params);
+        console.log("query:", req.query);
+        console.log("body:", req.body);
+        console.log("headers:", req.headers);
+        console.log("method:", req.method);
+        console.log("url:", req.url);
+        console.log(`User Id ${req.user}`)
+
+        res.send(`<h2>Welcome ${id} </h2>`);
+    })
+    .put((req, res) => {
+        const id = req.params.id;
+        console.log(`User ${id} is updated`)
+        res.send(`User ${id} is updated succesfully`)
+    })
+    .patch((req, res) => {
+        const id = req.params.id;
+        console.log("User  is partially updates")
+        res.send(`User ${id} is partially updated succesfully`)
+
+    })
+    .delete((req, res) => {
+        const id = req.params.id;
+        console.log("User is deleted")
+        res.send(`User ${id} is deleted succesfully`)
+    })
+
+const users = [{ name: "Jamal" }, { name: "Sally" }];
+
+router.param("id", (req, res, next, id) => {
+
+    console.log(`Advanced Routing Function with param id: ${id}`);
+    const userId = Number(id);
+    req.user = users[userId];
+    const account = req.user
+    console.log(`User Account ${JSON.stringify(account)}`);
+
+    next() //this routes to corresponding function
+})
+
+
+
 
 module.exports = router
